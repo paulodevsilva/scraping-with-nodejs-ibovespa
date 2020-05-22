61d274dac67d884e7310aa31bf3283aab933c582
paulodevsilva <silva.paulohr@gmail.com>
2020-05-22 20:59:09 -0300

    change a log of the server and remove body-parser

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 8080;
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', async (req, res) => {
-    res.send('Scraping bot')
-});
 
 require('./src/app');
 
